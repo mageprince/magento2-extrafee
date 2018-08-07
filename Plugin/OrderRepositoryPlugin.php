1a606945c6a60dfe7500d603ffc3d9e6c8eb8c18
Prince Patel <mail.mageprince@gmail.com>
2018-08-07 23:53:52 +0530

    Add extrafee in order api

---
+++ Plugin/OrderRepositoryPlugin.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Prince\Extrafee\Plugin;
+
+use Magento\Sales\Api\Data\OrderExtensionFactory;
+use Magento\Sales\Api\Data\OrderInterface;
+use Magento\Sales\Api\Data\OrderSearchResultInterface;
+use Magento\Sales\Api\OrderRepositoryInterface;
+
+class OrderRepositoryPlugin
+{
+    /**
+     * Extrafee field name
+     */
+    const FIELD_NAME = 'fee';
+
+    /**
+     * Order Extension Attributes Factory
+     *
+     * @var OrderExtensionFactory
+     */
+    protected $extensionFactory;
+
+    /**
+     * OrderRepositoryPlugin constructor
+     *
+     * @param OrderExtensionFactory $extensionFactory
+     */
+    public function __construct(OrderExtensionFactory $extensionFactory)
+    {
+        $this->extensionFactory = $extensionFactory;
+    }
+
+    /**
+     * Add "extrafee" extension attribute to order data object to make it accessible in API data
+     *
+     * @param OrderRepositoryInterface $subject
+     * @param OrderInterface $order
+     *
+     * @return OrderInterface
+     */
+    public function afterGet(OrderRepositoryInterface $subject, OrderInterface $order)
+    {
+        $extraFee = $order->getData(self::FIELD_NAME);
+        $extensionAttributes = $order->getExtensionAttributes();
+        $extensionAttributes = $extensionAttributes ? $extensionAttributes : $this->extensionFactory->create();
+        $extensionAttributes->setExtrafee($extraFee);
+        $order->setExtensionAttributes($extensionAttributes);
+
+        return $order;
+    }
+
+    /**
+     * Add "extrafee" extension attribute to order data object to make it accessible in API data
+     *
+     * @param OrderRepositoryInterface $subject
+     * @param OrderSearchResultInterface $searchResult
+     *
+     * @return OrderSearchResultInterface
+     */
+    public function afterGetList(OrderRepositoryInterface $subject, OrderSearchResultInterface $searchResult)
+    {
+        $orders = $searchResult->getItems();
+
+        foreach ($orders as &$order) {
+            $extraFee = $order->getData(self::FIELD_NAME);
+            $extensionAttributes = $order->getExtensionAttributes();
+            $extensionAttributes = $extensionAttributes ? $extensionAttributes : $this->extensionFactory->create();
+            $extensionAttributes->setExtrafee($extraFee);
+            $order->setExtensionAttributes($extensionAttributes);
+        }
+
+        return $searchResult;
+    }
+}
